5f8e3fd2e00e2a23d38713a49d7e3b62df2321aa
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-11-04 15:23:32 -0300

    menu criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, ThemeToggleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'bichinhos-da-ti-lp';
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [ThemeToggleComponent],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {}
