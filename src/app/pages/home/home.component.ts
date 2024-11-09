244478fa00edc37d151029f9a44cc497ef4d3a70
Robert Santos <robertdahora@gmail.com>
2024-11-09 09:45:45 -0300

    refactor: change component name to hero

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/layout/footer/footer.component';
 import { MenuComponent } from '../../components/layout/menu/menu.component';
-import { AboutUsComponent } from '../../components/about-us/about-us.component';
+import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, MenuComponent, AboutUsComponent],
+  imports: [FooterComponent, MenuComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
