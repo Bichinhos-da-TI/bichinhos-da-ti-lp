8ada34ed39470756895758846b9523b529fb7b1c
Robert Santos <robertdahora@gmail.com>
2024-11-09 09:24:39 -0300

    refactor: adjust folders and components name

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { FooterLinkComponent } from './components/footer-link/footer-link.component';
 import { CommonModule } from '@angular/common';
 import { SocialNetworkIconComponent } from './components/social-network-icon/social-network-icon.component';
+import {
+  contactText,
+  menuItems,
+  socialNetworks,
+} from '../../../../data/footer';
 
 @Component({
   selector: 'app-footer',
@@ -11,43 +16,7 @@ import { SocialNetworkIconComponent } from './components/social-network-icon/soc
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-  items = [
-    {
-      title: 'Inicio',
-      href: '#',
-    },
-    {
-      title: 'Quem somos',
-      href: '#quem-somos',
-    },
-    {
-      title: 'Nossos projetos',
-      href: '#projetos',
-    },
-    {
-      title: 'Depoimentos',
-      href: '#depoimentos',
-    },
-  ];
-
-  contactText =
-    'Tem dúvidas ou quer saber mais? Entre em contato conosco através do e-mail [email@example.com] ou siga-nos nas redes sociais para ficar por dentro das novidades';
-
-  socialNetworks = [
-    {
-      href: 'https://www.instagram.com/bichinhosdati/',
-      image: 'assets/instagram.svg',
-      alt: 'Instagram',
-    },
-    {
-      href: 'https://www.linkedin.com/company/bichinhosdati/',
-      image: 'assets/linkedin.svg',
-      alt: 'Linkedin',
-    },
-    {
-      href: '',
-      image: 'assets/discord.svg',
-      alt: 'Discord',
-    },
-  ];
+  menuItems = menuItems;
+  contactText = contactText;
+  socialNetworks = socialNetworks;
 }
---
+++ src/data/footer.ts
@@ -0,0 +1,39 @@
+export const menuItems = [
+  {
+    title: 'Inicio',
+    href: '#',
+  },
+  {
+    title: 'Quem somos',
+    href: '#quem-somos',
+  },
+  {
+    title: 'Nossos projetos',
+    href: '#projetos',
+  },
+  {
+    title: 'Depoimentos',
+    href: '#depoimentos',
+  },
+];
+
+export const contactText =
+  'Tem dúvidas ou quer saber mais? Entre em contato conosco através do e-mail [email@example.com] ou siga-nos nas redes sociais para ficar por dentro das novidades';
+
+export const socialNetworks = [
+  {
+    href: 'https://www.instagram.com/bichinhosdati/',
+    image: 'assets/instagram.svg',
+    alt: 'Instagram',
+  },
+  {
+    href: 'https://www.linkedin.com/company/bichinhosdati/',
+    image: 'assets/linkedin.svg',
+    alt: 'Linkedin',
+  },
+  {
+    href: '',
+    image: 'assets/discord.svg',
+    alt: 'Discord',
+  },
+];
