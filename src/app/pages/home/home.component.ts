db6ef039b96bd12aecbca4a642b2b3fd7a8ba7fd
Robert Santos <robertdahora@gmail.com>
2024-11-10 10:27:52 -0300

    refactor: various responsiveness optimizations

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../components/layout/footer/footer.component';
-import { MenuComponent } from '../../components/layout/menu/menu.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, MenuComponent, HeroComponent, AboutUsComponent],
+  imports: [HeroComponent, AboutUsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
 })
 export class HomeComponent {}
