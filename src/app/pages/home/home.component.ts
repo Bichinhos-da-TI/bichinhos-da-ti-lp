fcbca6c7c75dd4ea181aeae324ef21e2413447cf
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-11-25 18:09:10 -0300

    segunda secao iniciada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
+import { HighlightImpactProjectsComponent } from '../../components/highlight-impact-projects/highlight-impact-projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutUsComponent],
+  imports: [HeroComponent, AboutUsComponent, HighlightImpactProjectsComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
