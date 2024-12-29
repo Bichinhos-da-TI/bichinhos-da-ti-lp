4b323fd78fd610895bc1a88f4831fc6284770a51
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-12-29 13:35:31 -0300

    resolvendo conflitos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,17 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
 import { HighlightImpactProjectsComponent } from '../../components/highlight-impact-projects/highlight-impact-projects.component';
-import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeroComponent,
-    AboutUsComponent,
-    HighlightImpactProjectsComponent,
-    TestimonialsComponent,
-  ],
+  imports: [HeroComponent, AboutUsComponent, HighlightImpactProjectsComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
