98d38a57f540ff156cadefbb47641580e3afdf1c
Robert Santos <robertdahora@gmail.com>
2025-01-06 20:12:12 +0000

    feat: send form t o email

--- src/app/components/lead-form/lead-form.component.ts
+++ src/app/components/lead-form/lead-form.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import emailjs from '@emailjs/browser';
 
 import { CloseButtonComponent } from '../layout/close-button/close-button.component';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from '../layout/spinner/spinner.component';
 
 @Component({
   selector: 'app-lead-form',
   standalone: true,
-  imports: [CommonModule, CloseButtonComponent, OverlayModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    CloseButtonComponent,
+    OverlayModule,
+    MatProgressSpinnerModule,
+    SpinnerComponent,
+  ],
   templateUrl: './lead-form.component.html',
   styleUrl: './lead-form.component.css',
 })
@@ -15,8 +26,34 @@ export class LeadFormComponent {
   @Input() closeOverlay!: () => void;
 
   isOpen: boolean = false;
+  loading: boolean = false;
+
+  serviceId = _NGX_ENV_.NG_APP_EMAIL_SERVICE_ID;
+  templateId = _NGX_ENV_.NG_APP_EMAIL_TEMPLATE_ID;
+  publicKey = _NGX_ENV_.NG_APP_EMAIL_PUBLIC_KEY;
+
+  public sendEmail(e: Event) {
+    this.setLoading(true);
+
+    emailjs
+      .sendForm(this.serviceId, this.templateId, e.target as HTMLFormElement, {
+        publicKey: this.publicKey,
+      })
+      .then(() => {
+        this.closeOverlay();
+        this.setLoading(false);
+      });
+  }
 
   setIsOpen(value: boolean) {
     this.isOpen = value;
+  }
+
+  get isLoading() {
+    return this.loading;
+  }
+
+  setLoading(value: boolean) {
+    this.loading = value;
   }
 }
